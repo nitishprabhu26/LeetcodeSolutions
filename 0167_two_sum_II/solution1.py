ad61515b8219e5aa819f7ec6615c4244f61f5a1d
Nitish Prabhu Kota <nitish.prabhu26@gmail.com>
2021-05-15 20:49:00 -0500

    0167 Two sum II

---
+++ 0167_two_sum_II/solution1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def twoSum(self, numbers: [int], target: int) -> [int]:
+        low=0
+        high=len(numbers)-1
+        while(low<high):
+            sum=numbers[low]+numbers[high]
+            if target==sum:
+                return [low+1,high+1]
+            elif sum<target:
+                low += 1
+            else:
+                high -= 1
+
+nums = [2,7,11,15]
+target = 9
+obj = Solution()
+print(obj.twoSum(nums, target))
+
+# Complexity analysis:
+
+# Time complexity : O(n). Each of the n elements is visited at most once, thus the time complexity is O(n).
+# Space complexity : O(1). We only use two indexes, the space complexity is O(1).
