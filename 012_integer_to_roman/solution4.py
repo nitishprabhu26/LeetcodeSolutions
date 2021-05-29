830a201db8d88de80cac6d3f4bfa4eba23c898b8
Nitish Prabhu Kota <nitish.prabhu26@gmail.com>
2021-05-29 18:02:04 -0500

    12. Integer to Roman

--- 012_integer_to_roman/solution4.py
+++ 012_integer_to_roman/solution4.py
@@ -1,15 +1,15 @@
-# Using while loop
+# Using while loop - not a good solution
 class Solution:
     def intToRoman(self, num: int) -> str:
         d = {1000: 'M', 900: 'CM', 500: 'D', 400: 'CD', 100: 'C', 90: 'XC',
              50: 'L', 40: 'XL', 10: 'X', 9: 'IX', 5: 'V', 4: 'IV', 1: 'I'}
-        
-        ans=""  
-        
-        while(num>0):
+
+        ans = ""
+
+        while(num > 0):
             for i in d:
-                if i<=num:
-                    ans+=(d[i])*int(num/i)
+                if i <= num:
+                    ans += (d[i])*int(num/i)
                     num = num % i
                     break
         return ans
