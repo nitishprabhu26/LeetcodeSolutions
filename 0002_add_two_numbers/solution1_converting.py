53834010de686196e7f45ce6da18dcadf239aab2
Nitish Prabhu Kota <nitish.prabhu26@gmail.com>
2021-09-25 03:27:33 -0500

    2. Add Two Numbers

---
+++ 0002_add_two_numbers/solution1_converting.py
@@ -0,0 +1,33 @@
+# Not advisable. Here we are traversing both the linkedlists, converting them into 
+# the values for which we need to find the sum, now sum them and then later convert 
+# back the sum to linked list. 
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        num1=""
+        num2=""
+        while l1:
+            num1=str(l1.val)+num1
+            l1=l1.next
+        while l2:
+            num2=str(l2.val)+num2
+            l2=l2.next
+        out=str(int(num1)+int(num2))[::-1]
+        result=ListNode(0)
+        result_tail=result
+        print(result)
+        for i in out:
+            result_tail.next=ListNode(i)
+            result_tail=result_tail.next
+        return result.next
+
+
+l1 = [2,4,3]
+l2 = [5,6,4]
+obj = Solution()
+print(obj.addTwoNumbers(l1,l2))
