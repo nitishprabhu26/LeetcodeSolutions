af281f096568d70e51694e5cfc825234965328ee
Nitish Prabhu Kota <nitish.prabhu26@gmail.com>
2021-09-28 23:15:23 -0500

    4. Median of Two Sorted Arrays

---
+++ 0004_median_of_two_sorted_arrays/solution1_using_sort.py
@@ -0,0 +1,24 @@
+# Not advisable. Using sort function
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: int, nums2: int) -> float:
+        final_array = nums1 + nums2
+        final_array.sort()
+        med = 0
+        length = len(final_array)
+        if length % 2 == 0:
+            med = (final_array[length//2]+final_array[length//2-1])/2
+        else:
+            med = final_array[length//2]
+        return med
+
+
+nums1 = [1, 2]
+nums2 = [3, 4]
+obj = Solution()
+print(obj.findMedianSortedArrays(nums1, nums2))
+
+# Complexity Analysis:
+# Time complexity : O(n.logn). Sort is used.
+# Space complexity : O(m+n). extra space is used to store the final_array, 
+# and later sorting is applied to it
---
+++ 0004_median_of_two_sorted_arrays/solution2.py
@@ -0,0 +1,55 @@
+# Neetcode
+# https://youtu.be/q6IEA26hvXc
+# In question, The overall run time complexity should be O(log (m+n)) worst case.
+# So it is supposed to be binary search
+# Remember that the given input arrays are in sorted order
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: int, nums2: int) -> float:
+
+        A, B = nums1, nums2
+        total = len(nums1)+len(nums2)
+        half = total//2
+
+        # making A the array with less number of elements
+        if len(B) < len(A):
+            A, B = B, A
+
+        # Run binary search on A, i.e. for the small array
+        # Array has 0 indexing
+        l, r = 0, len(A)-1
+
+        # Theres a guarantee that there is a minimum
+        while True:
+            # i,j (pointer for A and B respectively) are index values
+            i = (l+r) // 2  # Middle value for A
+            j = half - i - 1 - 1  # B
+
+            # if condition - since indices could be out of bounds
+            # if array A has 0 values or if i<0
+            Aleft = A[i] if i >= 0 else float('-infinity')
+            # we want all the values in array A to be in the left partition
+            Aright = A[i+1] if (i+1) < len(A) else float('infinity')
+            Bleft = B[j] if j >= 0 else float('-infinity')
+            Bright = B[j+1] if (j+1) < len(B) else float('infinity')
+
+            # partition is done correctly, otherwise go to elif and else
+            if Aleft <= Bright and Bleft <= Aright:
+                # odd case (odd number of elements)
+                if total % 2:
+                    return min(Aright, Bright)
+                # even case (odd number of elements)
+                return (max(Aleft, Bleft) + min(Aright, Bright))/2
+            elif Aleft > Bright:
+                r = i-1
+            else:
+                l = i+1
+
+
+nums1 = [1, 2, 6, 6]
+nums2 = [3, 4]
+obj = Solution()
+print(obj.findMedianSortedArrays(nums1, nums2))
+
+# Complexity Analysis:
+# Time complexity : O(log min(m,n)). Assume that m and n represents the length of nums1 and nums2 respectively.
