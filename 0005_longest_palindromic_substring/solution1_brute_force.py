a4a839bdc8dc90f4ad862a375b074eec33f05dd9
Nitish Prabhu Kota <nitish.prabhu26@gmail.com>
2021-09-29 17:15:19 -0500

    5. Longest Palindromic Substring

---
+++ 0005_longest_palindromic_substring/solution1_brute_force.py
@@ -0,0 +1,9 @@
+# The obvious brute force solution is to pick all possible starting and ending positions for a substring, 
+# and verify if it is a palindrome.
+
+# Complexity Analysis:
+# Time complexity : O(n^3). 
+# Assume that n is the length of the input string, there are a total of n^2 such substrings (excluding the 
+# trivial solution where a character itself is a palindrome). 
+# Since verifying each substring takes O(n) time, the run time complexity is O(n^3).
+# Space complexity : O(1).
