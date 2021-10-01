891cf8d1e22a5931adc37e58b3fd24b964329f7e
Nitish Prabhu Kota <nitish.prabhu26@gmail.com>
2021-10-01 02:54:16 -0500

    198. House Robber

---
+++ 198_house_robber/solution3.py
@@ -0,0 +1,39 @@
+# Approach 2: Dynamic Programming
+
+# The idea here is the same as before except that instead of following a recursive approach, we will be sticking with a tabular approach. 
+# The recursive approach may run into trouble when the recursion stack grows too large. It may also run into trouble because, for each 
+# recursive call, the compiler must do additional work to maintain the call stack (function variables, etc.) which results in unwanted 
+# overhead. The dynamic programming approach is simply a tabular formulation of the ideas presented above.
+
+class Solution:
+    def rob(self, nums: [int]) -> int:
+        # Special handling for empty case.
+        if not nums:
+            return 0
+        
+        maxRobbedAmount = [None for _ in range(len(nums) + 1)]
+        N = len(nums)
+        
+        # Base case initialization.
+        maxRobbedAmount[N], maxRobbedAmount[N - 1] = 0, nums[N - 1]
+        
+        # DP table calculations.
+        for i in range(N - 2, -1, -1):
+            
+            # Same as recursive solution.
+            maxRobbedAmount[i] = max(maxRobbedAmount[i + 1], maxRobbedAmount[i + 2] + nums[i])
+            
+        return maxRobbedAmount[0]  
+        
+
+nums = [1,2,3,1]
+nums = [2,7,9,3,1]
+obj = Solution()
+print(obj.rob(nums))
+
+# Complexity Analysis:
+# Time Complexity: O(N) since we have a loop from N−2 ⋯ 0 and we simply use the pre-calculated values of our dynamic programming 
+# table for calculating the current value in the table which is a constant time operation.
+# Space Complexity: O(N) which is used by the table. So what is the real advantage of this solution over the previous solution? 
+# In this case, we don't have a recursion stack. When the number of houses is large, a recursion stack can become a serious limitation, 
+# because the recursion stack size will be huge and the compiler will eventually run into stack-overflow problems.
---
+++ 198_house_robber/solution4.py
@@ -0,0 +1,40 @@
+# Approach 3: Optimized Dynamic Programming
+
+# This is the exact same solution as the previous one with the exception that we will be optimizing the space complexity here.
+
+# To calculate the current value, we just need to rely on the next two consecutive values/recursive calls.
+# Instead of keeping an entire table for storing these cached values, we can get by with simply keeping track of the "next" two values.
+
+class Solution:
+    def rob(self, nums: [int]) -> int:
+        # Special handling for empty case.
+        if not nums:
+            return 0
+        
+        N = len(nums)
+        
+        rob_next_plus_one = 0
+        rob_next = nums[N - 1]
+        
+        # DP table calculations.
+        for i in range(N - 2, -1, -1):
+            
+            # Same as recursive solution.
+            current = max(rob_next, rob_next_plus_one + nums[i])
+            
+            # Update the variables
+            rob_next_plus_one = rob_next
+            rob_next = current
+            
+        return rob_next
+        
+
+nums = [1,2,3,1]
+nums = [2,7,9,3,1]
+obj = Solution()
+print(obj.rob(nums))
+
+# Complexity Analysis:
+# Time Complexity: O(N) since we have a loop from N−2⋯0 and we use the precalculated values of our dynamic programming table to 
+# calculate the current value in the table which is a constant time operation.
+# Space Complexity: O(1) since we are not using a table to store our values. Simply using two variables will suffice for our calculations.
