757cbd4025ee2c09b5afc82cc32a7eaabb7d8aa7
Nitish Prabhu Kota <nitish.prabhu26@gmail.com>
2022-01-13 02:39:36 -0600

    452. Minimum Number of Arrows to Burst Balloons

---
+++ 0452_minimum_number_of_arrows_to_burst_balloons/solution1.py
@@ -0,0 +1,41 @@
+# Approach 1: Greedy
+# https://leetcode.com/problems/minimum-number-of-arrows-to-burst-balloons/solution/ (followed this)
+# (use picture in example to understand)
+# OR
+# https://youtu.be/hND9YHeZJLA
+
+from typing import List
+
+
+class Solution:
+    def findMinArrowShots(self, points: List[List[int]]) -> int:
+        if not points:
+            return 0
+        
+        # sort by x_end
+        points.sort(key = lambda x : x[1])
+        
+        arrows = 1
+        first_end = points[0][1]
+        for x_start, x_end in points:
+            # if the current balloon starts after the end of another one,
+            # one needs one more arrow
+            if x_start > first_end:
+                arrows += 1
+                first_end = x_end
+        
+        return arrows
+
+points = [[10,16],[2,8],[1,6],[7,12]]
+
+obj = Solution()
+print(obj.findMinArrowShots(points))
+
+
+# Complexity Analysis:
+# Time complexity : O(NlogN) because of sorting of input data.
+# Space complexity : O(N) or O(logN)
+# The space complexity of the sorting algorithm depends on the implementation of each program language.
+# For instance, the list.sort() function in Python is implemented with the Timsort algorithm whose space 
+# complexity is O(N). In Java, the Arrays.sort() is implemented as a variant of quicksort algorithm whose 
+# space complexity is O(logN).
