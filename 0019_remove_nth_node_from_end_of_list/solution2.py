8faaada08a88eb3e350c68839376bfb6105b3d1e
Nitish Prabhu Kota <nitish.prabhu26@gmail.com>
2022-01-21 23:20:18 -0600

    checkin

--- 0019_remove_nth_node_from_end_of_list/solution2.py
+++ 0019_remove_nth_node_from_end_of_list/solution2.py
@@ -2,10 +2,11 @@
 
 # Approach 2: One pass algorithm
 # Algorithm:
-# The previous algorithm could be optimized to one pass. Instead of one pointer, we could use two pointers. The first pointer advances
-# the list by n+1 steps from the beginning, while the second pointer starts from the beginning of the list. Now, both pointers are
-# exactly separated by n nodes apart. We maintain this constant gap by advancing both pointers together until the first pointer arrives
-# past the last node. The second pointer will be pointing at the nth node counting from the last. We relink the next pointer of the
+# The previous algorithm could be optimized to one pass. Instead of one pointer, we could use two pointers. 
+# The first pointer advances the list by n+1 steps from the beginning, while the second pointer starts from 
+# the beginning of the list. Now, both pointers are exactly separated by n nodes apart. We maintain this 
+# constant gap by advancing both pointers together until the first pointer arrives past the last node. The 
+# second pointer will be pointing at the nth node counting from the last. We relink the next pointer of the
 # node referenced by the second pointer to point to the node's next next node.
 
 # Definition for singly-linked list.
@@ -41,5 +42,6 @@ obj = Solution()
 print(obj.removeNthFromEnd(head, n))
 
 # Complexity Analysis:
-# Time complexity : O(L).The algorithm makes one traversal of the list of L nodes. Therefore time complexity is O(L).
+# Time complexity : O(L).The algorithm makes one traversal of the list of L nodes. Therefore time complexity is 
+# O(L).
 # Space complexity : O(1). We only used constant extra space.
