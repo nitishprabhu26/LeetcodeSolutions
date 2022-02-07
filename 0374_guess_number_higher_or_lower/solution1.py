a8cf6470cf188265a0446b68621de1100ccf5334
Nitish Prabhu Kota <nitish.prabhu26@gmail.com>
2022-02-07 06:55:31 -0600

    374. Guess Number Higher or Lower

---
+++ 0374_guess_number_higher_or_lower/solution1.py
@@ -0,0 +1,25 @@
+# Approach 1: Brute Force
+# We check every number from 1 to n-1 and pass it to the guess function. The number for which a 0 is returned 
+# is the required answer.
+
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+# def guess(num: int) -> int:
+
+class Solution:
+    def guessNumber(self, n: int) -> int:
+        for i in range(1, n+1):
+            if guess(i) == 0:
+                return i
+
+
+n = 10
+pick = 6
+obj = Solution()
+print(obj.guessNumber(n))
+
+
+# Complexity Analysis:
+# Time complexity : O(n). We scan all the numbers from 1 to n.
+# Space complexity : O(1). No extra space is used.
---
+++ 0374_guess_number_higher_or_lower/solution2.py
@@ -0,0 +1,37 @@
+# Approach 2: Using Binary Search
+# https://youtu.be/xW4QsTtaCa4
+# We can apply Binary Search to find the given number. We start with the mid number. We pass that number to the 
+# guess function. If it returns a -1, it implies that the guessed number is larger than the required one. Thus, 
+# we use Binary Search for numbers lower than itself. Similarly, if it returns a 1, we use Binary Search for 
+# numbers higher than itself.
+
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+# def guess(num: int) -> int:
+
+class Solution:
+    def guessNumber(self, n: int) -> int:
+        low, high = 1, n
+        
+        while low <= high:
+            mid = low + (high - low) // 2
+            res = guess(mid)
+            
+            if res > 0:
+                low = mid + 1
+            elif res < 0:
+                high = mid - 1
+            else:
+                return mid
+
+
+n = 10
+pick = 6
+obj = Solution()
+print(obj.guessNumber(n))
+
+
+# Complexity Analysis:
+# Time complexity : O(log n). Binary Search is used.
+# Space complexity : O(1). No extra space is used.
