ce99a95cfe1010872310a1f57299690730104412
Nitish Prabhu Kota <nitish.prabhu26@gmail.com>
2022-04-05 19:28:56 -0500

    49. Group Anagrams

---
+++ 0049_group_anagrams/solution1.py
@@ -0,0 +1,63 @@
+# Approach 1: Categorize by Sorted String
+
+# Intuition:
+# Two strings are anagrams if and only if their sorted strings are equal.
+
+# Algorithm:
+# Maintain a map ans : {String -> List} 
+# where each key K is a sorted string, and each value is the list of strings from the initial input that when 
+# sorted, are equal to K.
+
+# In Java, we will store the key as a string, eg. code. 
+# In Python, we will store the key as a hashable tuple, eg. ('c', 'o', 'd', 'e').
+
+
+import collections
+from typing import List
+
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        ans = collections.defaultdict(list)
+        for s in strs:
+            ans[tuple(sorted(s))].append(s)
+        return ans.values()
+
+
+# OR
+# storing key as string in Python
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        ans = collections.defaultdict(list)
+        for s in strs:
+            ans[''.join(sorted(s))].append(s)
+        return ans.values()
+
+
+# OR Using regular dictionary
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        dict={}
+        for i in strs:
+            k=''.join(sorted(i))
+
+            if k in dict:
+                dict[k]+=[i]
+            else:
+                dict[k]=[i]
+
+        return dict.values()
+
+
+strs = ["eat","tea","tan","ate","nat","bat"]
+obj = Solution()
+print(obj.groupAnagrams(strs))
+
+
+# Complexity Analysis:
+# Time complexity : O(N.K.logK), where N is the length of strs, and K is the maximum length of a string in strs. 
+# The outer loop has complexity O(N) as we iterate through each string. Then, we sort each string in 
+# O(KlogK) time.
+# Space complexity : O(N.K), the total information content stored in ans. 
---
+++ 0049_group_anagrams/solution2.py
@@ -0,0 +1,42 @@
+# Approach 2: Categorize by Count
+# OR
+# Neetcode : https://youtu.be/vzdNOK2oB2E
+
+# Intuition:
+# Two strings are anagrams if and only if their character counts (respective number of occurrences of each 
+# character) are the same.
+
+# Algorithm:
+# We can transform each string s into a character count, count, consisting of 26 non-negative integers 
+# representing the number of a's, b's, c's, etc. We use these counts as the basis for our hash map.
+
+# In python, the representation will be a tuple of the counts. For example, abbccc will be (1, 2, 3, 0, 0, ..., 0),
+# where again there are 26 entries total.
+
+
+import collections
+from typing import List
+
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        ans = collections.defaultdict(list)
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            # list cannot be key of dict, so we use tuple which is non mutable
+            ans[tuple(count)].append(s)
+        return ans.values()
+
+
+
+strs = ["eat","tea","tan","ate","nat","bat"]
+obj = Solution()
+print(obj.groupAnagrams(strs))
+
+
+# Complexity Analysis:
+# Time complexity : O(N.K) i.e. O(26.N.K), where N is the length of strs, and K is the maximum length of a string 
+# in strs. Counting each string is linear in the size of the string, and we count every string.
+# Space complexity : O(N.K), the total information content stored in ans. 
