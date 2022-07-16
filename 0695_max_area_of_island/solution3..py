a1087c6fbbbdf17cb520b761087bcef98ee4768f
Nitish Prabhu Kota <nitish.prabhu26@gmail.com>
2022-07-16 04:55:27 -0500

    695. Max Area of Island

--- 0695_max_area_of_island/solution3..py
+++ 0695_max_area_of_island/solution3..py
@@ -39,6 +39,32 @@ obj = Solution()
 print(obj.maxAreaOfIsland(grid))
 
 
+# OR
+# Set grid[r][c] == 0 when visited, instead of cisit hashset
+
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
+
+        def dfs(r, c):
+            if (r < 0 or r == ROWS or c < 0 or c == COLS or
+                grid[r][c] == 0):
+                    return 0
+            grid[r][c] = 0
+            return (1 + dfs(r - 1, c) +
+                        dfs(r + 1, c) +
+                        dfs(r, c - 1) +
+                        dfs(r, c + 1))
+
+        area = 0
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 1:
+                    area = max(area, dfs(r, c))
+        
+        return area
+
+
 # Complexity Analysis:
 # Time complexity : O(R ∗ C), where R is the number of rows in the given grid, and C is the number of columns. We 
 # visit every square once.
