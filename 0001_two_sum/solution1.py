887c25c33e1e749977f361178d1f4d4516b22b85
Nitish Prabhu Kota <nitish.prabhu26@gmail.com>
2022-08-14 15:59:37 -0500

    1. Two Sum

---
+++ 0001_two_sum/solution1.py
@@ -0,0 +1,38 @@
+# Approach 1: Brute Force
+
+# Algorithm:
+# The brute force approach is simple. Loop through each element x and find if there is another value that equals 
+# to target - x.
+
+
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(0, len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return i, j
+
+# OR
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        listIndices=[]
+        for i in range(len(nums)):
+            for j in range(i + 1,len(nums)):
+                if nums[i] + nums[j] == target:
+                    listIndices.append(i)
+                    listIndices.append(j)
+                    return listIndices
+
+                    
+nums = [2, 6, 11, 15]
+target = 8
+obj = Solution()
+print(obj.twoSum(nums, target))
+
+
+# Complexity Analysis:
+# Time complexity : O(n^2)
+# Space complexity : O(1)
--- 0001_two_sum/solution2.py
+++ 0001_two_sum/solution2.py
@@ -1,42 +1,40 @@
-# Approach 3: One-pass Hash Table:
+# Approach 2: Two-pass Hash Table:
 
-# It turns out we can do it in one-pass. While we iterate and inserting elements into the table, we also look 
-# back to check if current element's complement already exists in the table. If it exists, we have found a 
-# solution and return immediately.
- 
+# To improve our run time complexity, we need a more efficient way to check if the complement exists in the 
+# array. 
+# If the complement exists, we need to look up its index. What is the best way to maintain a mapping of each 
+# element in the array to its index? A hash table.
+
+# We reduce the look up time from O(n) to O(1) by trading space for speed. A hash table is built exactly for 
+# this purpose, it supports fast look up in near constant time. 
+
+# A simple implementation uses two iterations. In the first iteration, we add each element's value and its 
+# index to the table. Then, in the second iteration we check if each element's complement (target−nums[i]) 
+# exists in the table. 
+# Beware that the complement must not be nums[i] itself!
+
 
 from typing import List
-
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         dict = {}
-        for i, x in enumerate(nums):
-            y = target - x
-            if y in dict:
-                return [dict[y], i]
-            else:
-                dict[x] = i
+        for i in range(len(nums)):
+            dict[nums[i]]=i
+        for i in range(len(nums)):
+            y = target - nums[i]
+            if y in dict and dict[y]!=i:
+                return [i, dict[y]]
 
-# or
 
-# class Solution:
-#     def twoSum(self, nums: [int], target: int) -> [int]:
-#         dict1 = {}
-#         for i in range(0, len(nums)):
-#             if target - nums[i] in dict1:
-#                 return [dict1[target - nums[i]], i]
-#             else:
-#                 dict1[nums[i]] = i
-
-nums = [5,2,2,2,7,5,3,7,4,5,4]
-target = 9
+nums = [2, 6, 11, 15]
+target = 8
 obj = Solution()
 print(obj.twoSum(nums, target))
 
 
 # Complexity Analysis:
-# Time complexity : O(n). We traverse the list containing n elements only once. Each look up in the table costs 
-# only O(1) time.
+# Time complexity : O(n). We traverse the list containing n elements exactly twice. Since the hash table 
+# reduces the look up time to O(1), the time complexity is O(n).
 # Space complexity : O(n). The extra space required depends on the number of items stored in the hash table, 
-# which stores at most n elements.
+# which stores exactly n elements.
