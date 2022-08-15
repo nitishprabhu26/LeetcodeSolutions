d2f3df186f09230b622c021a3fb3482ed77fbab8
Nitish Prabhu Kota <nitish.prabhu26@gmail.com>
2022-08-14 22:54:12 -0500

    653. Two Sum IV - Input is a BST

--- 0653_two_sum_IV-input_is_a_BST/solution2.py
+++ 0653_two_sum_IV-input_is_a_BST/solution2.py
@@ -40,7 +40,23 @@ class Solution:
                 q.append(node.left)
         
         return False
-                    
+
+
+# OR
+# https://leetcode.com/problems/two-sum-iv-input-is-a-bst/discuss/106067/C%2B%2BPython-Straight-Forward-Solution
+
+
+class Solution:        
+    def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
+        if not root: return False
+        bfs, s = [root], set()
+        for i in bfs:
+            if k - i.val in s: return True
+            s.add(i.val)
+            if i.left: bfs.append(i.left)
+            if i.right: bfs.append(i.right)
+        return False
+
 
 root = [5,3,6,2,4,None,7]
 k = 9
