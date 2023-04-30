7f7dcab9d93b66c437408893b78839764e54bbfb
Nitish Prabhu Kota <nitish.prabhu26@gmail.com>
2023-04-30 03:58:18 -0500

    67. Add Binary

--- 0067_add_binary/solution1.py
+++ 0067_add_binary/solution1.py
@@ -11,11 +11,17 @@ class Solution:
     def addBinary(self, a: str, b: str) -> str:
         return '{0:b}'.format(int(a, 2) + int(b, 2))
 
+# OR
+
 # class Solution:
 #     def addBinary(self, a: str, b: str) -> str:
 #         return bin(int(a, 2) + int(b, 2))[2:]
+
 
 a = "11"
 b = "1"
 obj = Solution()
 print(obj.addBinary(a, b))
+
+
+# The overall algorithm has O(N+M) time complexity.
--- 0067_add_binary/solution4.py
+++ 0067_add_binary/solution4.py
@@ -1,12 +1,13 @@
 # Approach 2: Bit Manipulation
 
 # Algorithm:
-# Convert a and b into integers x and y, x will be used to keep an answer, and y for the carry.
-# While carry is nonzero: y != 0:
-# - Current answer without carry is XOR of x and y: answer = x^y.
-# - Current carry is left-shifted AND of x and y: carry = (x & y) << 1.
-# - Job is done, prepare the next loop: x = answer, y = carry.
-# Return x in the binary form.
+# - Convert a and b into integers x and y, x will be used to keep an answer, and y for the carry.
+# - While carry is nonzero: y != 0:
+#   - Current answer without carry is XOR of x and y: answer = x^y.
+#   - Current carry is left-shifted AND of x and y: carry = (x & y) << 1.
+#   - Job is done, prepare the next loop: x = answer, y = carry.
+# - Return x in the binary form.
+
 
 # class Solution:
 #     def addBinary(self, a, b) -> str:
@@ -17,6 +18,7 @@
 #             x, y = answer, carry
 #         return bin(x)[2:]
     
+
 class Solution:
     def addBinary(self, a, b) -> str:
         x, y = int(a, 2), int(b, 2)
