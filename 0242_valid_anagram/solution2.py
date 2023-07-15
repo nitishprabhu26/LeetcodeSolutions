fe4f95ef5efad290ddda0b4f09a51cefc48b92bb
Nitish Prabhu Kota <nitish.prabhu26@gmail.com>
2023-07-15 13:44:58 -0400

    242. Valid Anagram

--- 0242_valid_anagram/solution2.py
+++ 0242_valid_anagram/solution2.py
@@ -27,11 +27,3 @@ print(obj.isAnagram(s, t))
 # Time complexity : O(n). for loop
 # Space complexity : O(n). to store dictionary. but since we have 26 charecters in alphabets, it could be 
 # considered as O(1)
-
-
-# Follow up:
-# What if the inputs contain unicode characters? How would you adapt your solution to such case?
-# Answer:
-# Use a hash table instead of a fixed size counter. Imagine allocating a large size array to fit the entire range 
-# of unicode characters, which could go up to more than 1 million. A hash table is a more generic solution and 
-# could adapt to any range of characters.
--- 0242_valid_anagram/solution3.py
+++ 0242_valid_anagram/solution3.py
@@ -18,7 +18,6 @@ class Solution:
         return True
 
 # OR
-
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
@@ -36,6 +35,24 @@ class Solution:
 
         return True
 
+# OR
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        counter_array = [0]*26
+
+        for i in range(len(s)):
+            counter_array[ord(s[i]) - ord('a')] += 1
+
+        for i in range(len(t)):
+            if counter_array[ord(t[i]) - ord('a')] == 0:
+                return False
+            counter_array[ord(t[i]) - ord('a')] -= 1
+
+        return True
+    
 
 s = "anagram"
 t = "nagaram"
@@ -46,3 +63,11 @@ print(obj.isAnagram(s, t))
 # Complexity analysis:
 # Time complexity : O(n). for loop
 # Space complexity :  we have 26 charecters in alphabets, it could be considered as O(1) for the list.
+
+
+# Follow up:
+# What if the inputs contain unicode characters? How would you adapt your solution to such case?
+# Answer:
+# Use a hash table instead of a fixed size counter. Imagine allocating a large size array to fit the entire range 
+# of unicode characters, which could go up to more than 1 million. A hash table is a more generic solution and 
+# could adapt to any range of characters.
