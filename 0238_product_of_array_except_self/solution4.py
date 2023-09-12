608b786669161f0dbbf5a1bf26586ee4724f2db8
Nitish Prabhu Kota <nitish.prabhu26@gmail.com>
2023-09-11 23:43:13 -0400

    238. Product of Array Except Self

--- 0238_product_of_array_except_self/solution4.py
+++ 0238_product_of_array_except_self/solution4.py
@@ -57,6 +57,27 @@ class Solution:
         
         return answer
 
+# OR
+
+# Approach : Neetcode (Similar)
+# https://youtu.be/bNvIQI2wAjk
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res = [1] *len(nums)
+
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+
+        postfix = 1
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+
+        return res
+    
 
 nums = [1,2,3,4]
 obj = Solution()
