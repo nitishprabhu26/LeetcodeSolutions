70f898d7b979f270f86878b12337502be4a6289c
Nitish Prabhu Kota <nitish.prabhu26@gmail.com>
2024-02-04 12:09:24 -0500

    2. Add Two Numbers

--- 0002_add_two_numbers/solution1_converting.py
+++ 0002_add_two_numbers/solution1_converting.py
@@ -13,13 +13,13 @@ class Solution:
         num1 = ""
         num2 = ""
         while l1:
-            num1 = str(l1.val)+num1
+            num1 = str(l1.val) + num1
             l1 = l1.next
         while l2:
-            num2 = str(l2.val)+num2
+            num2 = str(l2.val) + num2
             l2 = l2.next
 
-        out = str(int(num1)+int(num2))[::-1]
+        out = str(int(num1) + int(num2))[::-1]
         result = ListNode(0)
         result_tail = result
         
