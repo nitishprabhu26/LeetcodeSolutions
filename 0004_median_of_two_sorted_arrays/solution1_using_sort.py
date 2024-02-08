452b912fe68a4ef40274de505e23612addec5afd
Nitish Prabhu Kota <nitish.prabhu26@gmail.com>
2024-02-07 20:56:04 -0500

    4. Median of Two Sorted Arrays

--- 0004_median_of_two_sorted_arrays/solution1_using_sort.py
+++ 0004_median_of_two_sorted_arrays/solution1_using_sort.py
@@ -1,4 +1,4 @@
-# Not advisable. Using sort function
+# Approach1: Using sort function (Not advisable.)
 
 class Solution:
     def findMedianSortedArrays(self, nums1: int, nums2: int) -> float:
@@ -7,7 +7,7 @@ class Solution:
         med = 0
         length = len(final_array)
         if length % 2 == 0:
-            med = (final_array[length//2]+final_array[length//2-1])/2
+            med = (final_array[length//2] + final_array[length//2-1])/2
         else:
             med = final_array[length//2]
         return med
@@ -20,5 +20,5 @@ print(obj.findMedianSortedArrays(nums1, nums2))
 
 
 # Complexity Analysis:
-# Time complexity : O(n.logn). Sort is used.
+# Time complexity : O((m+n).log(m+n)). Sort is used on the combined array.
 # Space complexity : O(m+n). extra space is used to store the final_array, and later sorting is applied.
