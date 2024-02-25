7d8b70295c219831b8c4adae125dd4d0c890b91c
Nitish Prabhu Kota <nitish.prabhu26@gmail.com>
2024-02-24 20:56:03 -0500

    191. Number of 1 Bits

--- 0191_number_of_1_bits/solution1.py
+++ 0191_number_of_1_bits/solution1.py
@@ -1,12 +1,17 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
         return bin(n).count("1")
-            
-            
+    
+
 n = 0b11111111111111111111111111111101
 obj = Solution()
 print(obj.hammingWeight(n))
 
+
 # Complexity Analysis:
-# Time Complexity: O(1)
-# Space Complexity: O(1)
+# Time Complexity: O(n).
+# bin() method - O(log n), where 'n' is the integer that is being converted.
+# This method needs to iterate through the bits of the integer to create the binary representation. The number 
+# of bits in the binary representation is proportional to the logarithm (base 2) of the integer value.
+# count() method - O(n), where 'n' is the length of the iterable. 
+# Space Complexity: O(1).
