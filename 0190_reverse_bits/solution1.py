3cf28f618c561d9feea87fffa90855a02747622b
Nitish Prabhu Kota <nitish.prabhu26@gmail.com>
2024-02-28 08:35:06 -0500

    190. Reverse Bits

--- 0190_reverse_bits/solution1.py
+++ 0190_reverse_bits/solution1.py
@@ -9,9 +9,15 @@
 # In order to combine the results of reversed bits (e.g. 2^a, 2^b), one could either use the addition operation
 # (i.e. 2^a + 2^b) or again the bit OR operation (i.e. 2^a | 2^b)
 
-# Algorithm
+# Algorithm:
 # The key idea is that for a bit that is situated at the index i, after the reversion, its position should be 
 # 31-i (note: the index starts from zero).
+# - We iterate through the bit string of the input integer, from right to left (i.e. n = n >> 1). To retrieve 
+#   the right-most bit of an integer, we apply the bit AND operation (n & 1).
+# - For each bit, we reverse it to the correct position (i.e. (n & 1) << power). Then we accumulate this 
+#   reversed bit to the final result.
+# - When there is no more bits of one left (i.e. n == 0), we terminate the iteration.
+
 
 class Solution:
     def reverseBits(self, n: int) -> int:
@@ -22,8 +28,8 @@ class Solution:
             power -= 1
         return res
             
-# &&
-
+# OR
+    
 # class Solution:
 #     def reverseBits(self, n: int) -> int:
 #         res = 0
