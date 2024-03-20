11472a638f9c3fba9c4635437b136db2d4ffd753
Nitish Prabhu Kota <nitish.prabhu26@gmail.com>
2024-03-20 08:53:47 -0400

    206. Reverse Linked List

--- 0206_reverse_linked_list/solution2.py
+++ 0206_reverse_linked_list/solution2.py
@@ -41,6 +41,27 @@ class Solution:
          
         return newHead
 
+# OR
+# Neetcode: https://youtu.be/G0_I-ZF0S38
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return None
+        
+        newHead = head
+        if head.next:
+            newHead = self.reverseList(head.next)
+            head.next.next = head    
+            head.next = None
+         
+        return newHead
+    
 
 nums = [1,2,3,4,5]
 obj = Solution()
